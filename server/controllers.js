78e298a62ad1ccdae776122946dee1b530dbb62e
Daniel Flores <danielflores125@gmail.com>
2021-09-28 22:59:11 -0700

    Found tax rate. next add up item total plus tax rates

--- server/controllers.js
+++ server/controllers.js
@@ -4,13 +4,19 @@ function getOrderDetails(orderID) {
   return axios.get(`https://code-challenge-i2hz6ik37a-uc.a.run.app/orders/${orderID}`);
 }
 
+function findTaxRate(zip) {
+  axios.get(`https://code-challenge-i2hz6ik37a-uc.a.run.app/cities/${zip}`)
+    .then((res) => res.data.tax_rate)
+    .catch((err) => err);
+}
+
 function calculateOrderTotal(req, res) {
   const orderOutput = { Order: req.params.id };
 
   getOrderDetails(orderOutput.Order)
     .then((details) => {
       orderOutput['Customer Name'] = details.data.shipping_name;
-      
+      const taxRate = findTaxRate(details.data.shipping_city);
       res.send(details.data);
     })
     .catch((err) => {
